f3cf5d61d6a76734d4e890f81b4f47a3034e3a8f
Nikhil Nalande <nalandenv@gmail.com>
2021-08-08 17:55:42 +0530

    add working form to iframe and implement sandbox

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Connected");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const name = document.querySelector("#name");
+  const age = document.querySelector("#age");
+  const list = document.querySelector("#list");
+  const h1 = document.createElement("h1");
+  h1.innerText = `Name: ${name.value} | Age: ${age.value}`;
+  list.append(h1);
+});
